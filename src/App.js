11a94f0e135ef384958eaa8252bcc300c59ba8e3
kprice31 <54342102+kprice31@users.noreply.github.com>
2021-08-14 21:41:56 -0600

    adding bootstrap and creating new class names for the container

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Kelsi Price and is{" "}
-        <a href="https://github.com/kprice31/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kelsi Price and is{" "}
+          <a
+            href="https://github.com/kprice31/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
