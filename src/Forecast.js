4623750757aa1a2067f98da1de696ffc9c74d3e1
kprice31 <54342102+kprice31@users.noreply.github.com>
2021-08-30 21:34:22 -0600

    revising

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
 import "./Forecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -17,29 +16,35 @@ export default function Forecast(props) {
     setLoaded(true);
   }
 
-  if (loaded) {
-    return (
-      <div className="Forecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
-          })}
-        </div>
-      </div>
-    );
-  } else {
+  function load() {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coord.lon;
     let latitude = props.coord.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
 
     return null;
   }
